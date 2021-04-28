aa9c8bb03111cf57b928fbc1d6b02ea3304aec3a
Ignacio <jalfaros@estudiantec.cr>
2021-04-28 15:17:16 -0600

    FormBuilder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'login',
+    component: LoginPageComponent
   },
   {
-    path: 'message/:id',
-    loadChildren: () => import('./view-message/view-message.module').then( m => m.ViewMessagePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'login'
+  }
 ];
+
+
 
 @NgModule({
   imports: [
---
+++ src/app/components/login-page/login-page.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss'],
+})
+export class LoginPageComponent implements OnInit {
+
+  forma: FormGroup
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
